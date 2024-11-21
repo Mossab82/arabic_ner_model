762e271646e069855e6dbdb4471224e02a112078
Mossab Ibrahim <47082589+Mossab82@users.noreply.github.com>
2024-11-21 12:24:25 +0300

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,9 @@
+"""
+Models module for Arabic NER.
+Provides implementations of CRF and rule-based models for NER tasks.
+"""
 
+from .crf_model import CRFModel
+from .rule_based import RuleBasedModel
+
+__all__ = ['CRFModel', 'RuleBasedModel']
